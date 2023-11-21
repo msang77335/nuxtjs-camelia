bcfb613da25be44e10c30924a6d0a98eb78136f4
msang77335@gmail.com <sang.nguyen@aegona.com>
2023-11-21 21:29:07 +0700

    Create Default Layout

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper default-layout">
+    <Header />
+    <Banner />
+    <slot />
+    <Footer />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.default-layout {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 100vh;
+}
+</style>
